a370305876f5317e9f8a7c45b462e76dbe411928
Ahmed <ahmed@turkyol.com>
2024-03-20 00:37:21 +0300

    feat: Enable App Authentication
    Part of SUH-29

--- install.sh
+++ install.sh
@@ -21,13 +21,17 @@ git clone git@github.com:suhailorg/docker-suhail-meet.git
 cd docker-suhail-meet || exit
 
 # Submodules
-git submodule update --init --recursive
-
+git submodule update --init --recursive --remote
 # Copy env.example to .env (no loading or validation here)
 cp env.example .env
 
-# Set PUBLIC_URL in .env (coming from the Stage 1 .env file)
+# Set ENV variables in .env (coming from the Stage 1 .env file)
 sed -i "s/#PUBLIC_URL=https:\/\/meet.example.com/PUBLIC_URL=https:\/\/$DOMAIN_NAME/" .env
+sed -i "s/#ENABLE_AUTH=1/ENABLE_AUTH=1" .env
+sed -i "s/#ENABLE_GUESTS=1/ENABLE_GUESTS=1" .env
+sed -i "s/#AUTH_TYPE=internal/AUTH_TYPE=jwt" .env
+sed -i "s/#JWT_APP_ID=my_jitsi_app_id/JWT_APP_ID=$JWT_APP_ID" .env
+sed -i "s/#JWT_APP_SECRET=my_jitsi_app_secret/JWT_APP_SECRET=$JWT_APP_SECRET" .env
 
 # Run ./gen-passwords.sh
 chmod +x ./gen-passwords.sh
